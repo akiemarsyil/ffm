30722d84ef5fb04a4d88600d699f8d6c94acf0e1
swhp <sw_hp@yahoo.com>
2014-10-07 05:56:24 +0700

    Modul client, edit template general header

--- application/modules/client/views/general/header.php
+++ application/modules/client/views/general/header.php
@@ -5,3 +5,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="robots" content="noindex, follow" />
 <meta name="author" content="" />
+
+<!-- CSS -->
+<link href="<?php echo base_url().'public/assets/css/bootstrap.css';?>" rel="stylesheet" type="text/css" media="screen"/>
+
+<!-- JS -->
+<script src="<?php echo base_url().'public/assets/js/jquery-1.11.1.js';?>" type="text/javascript"></script>
+<script src="<?php echo base_url().'public/assets/js/bootstrap.js';?>" type="text/javascript"></script>
